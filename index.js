b64078dd269c4ece4b46c8f79017d0ecdf04d89b
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-27 11:27:58 -0400

    added photos from json file

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello zowie')
+async function getPhotos() {
+    const response = await fetch("photos.json");
+    const photos = await response.json();
+    return photos
+}
+
+getPhotos().then(photos => {
+    console.log(photos)
+    let photosHtml = photos.map(photo => {
+        return `<img src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}" />`
+    }).join('')
+
+    console.log(photosHtml)
+    document.body.innerHTML = photosHtml;
+})
+
+// getPhotos();
