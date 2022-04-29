ef0e023672d3d6f57ac4c5c451b4e6fa8b7897ba
Zowie Erickson <zowie.erickson@gmail.com>
2022-04-28 21:12:55 -0400

    added main-photo that will be displayed when another photo is clicked

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ async function getPhotos() {
     return photos
 }
 
-getPhotos().then(photos => {
-    document.body.innerHTML = `<div class="my-gallery">${getPhotosHtml(photos)}</div>`;
-})
-
-/* 
-    2. 
-       Write a function to create the my-photos Component
-       
-       Create a Column Flexbox container around my-photos
-       
+/*    
        Create a new img and place it, centered, above my-photos
         -This should be hidden initially
         -Also give it the my-photo class
@@ -23,12 +14,26 @@ getPhotos().then(photos => {
 */
 
 function getPhotosHtml(photos) {
-    console.log(photos)
     let photosHtml = photos.map(photo => {
-        return `<img src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}" />`
+        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}" />`
     }).join('')
 
-    console.log(photosHtml)
-    // document.body.innerHTML = photosHtml;
     return `<div class="my-photos">${photosHtml}</div>`
 }
+
+getPhotos().then(photos => {
+    document.body.innerHTML = `
+    <div class="my-gallery">
+        <img class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/1/200/200" />
+        ${getPhotosHtml(photos)}
+    </div>`;
+    const myPhotoImgs = Array.from(document.querySelectorAll('.my-photo'));
+
+    myPhotoImgs.forEach(photoImg => {
+        photoImg.addEventListener("click", e => {
+            console.log(e.target)
+        })
+    })
+    console.log(myPhotoImgs)
+    
+})
